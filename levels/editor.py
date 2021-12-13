fed496354c7df56b97e20c558b89f133127d1fb7
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2021-12-13 23:33:21 +0100

    Editor: can resize level window and move around

--- levels/editor.py
+++ levels/editor.py
@@ -158,21 +158,47 @@ class _LevelWindow(pyglet.window.Window):
     def __init__(self, level: _Level, tileset: _Tileset, on_close: Callable) -> None:
         self._level = level
         self._on_close_callback = on_close
+        self._origin_x = 0
+        self._origin_y = 0
         self._tileset = tileset
-        super().__init__(caption='Level', width=(level.width_in_tiles() * tileset.tiles_size_in_pixels()),
-                         height=(level.height_in_tiles() * tileset.tiles_size_in_pixels()))
+        super().__init__(caption='Level', width=319, height=160,
+                         resizable=True)
+        self.set_maximum_size(level.width_in_tiles() * tileset.tiles_size_in_pixels(),
+                              level.height_in_tiles() * tileset.tiles_size_in_pixels())
+
+    def _level_height_in_pixels(self) -> int:
+        return self._level.height_in_tiles() * self._tileset.tiles_size_in_pixels()
+
+    def _level_width_in_pixels(self) -> int:
+        return self._level.width_in_tiles() * self._tileset.tiles_size_in_pixels()
 
     def on_close(self) -> None:
         self._on_close_callback()
 
     def on_draw(self) -> None:
         self.clear()
-        for y in range(0, self._level.height_in_tiles()):
-            for x in range(0, self._level.width_in_tiles()):
-                tile_index = self._level.tile_index(x, y)
+        origin_i = self._origin_x // self._tileset.tiles_size_in_pixels()
+        origin_j = self._origin_y // self._tileset.tiles_size_in_pixels()
+        last_i = (self._origin_x + self.width - 1) // self._tileset.tiles_size_in_pixels()
+        last_j = (self._origin_y + self.height - 1) // self._tileset.tiles_size_in_pixels()
+        for j in range(origin_j, last_j + 1):
+            for i in range(origin_i, last_i + 1):
+                tile_index = self._level.tile_index(i, j)
                 tile = self._tileset.tile(tile_index)
-                tile.blit(x * self._tileset.tiles_size_in_pixels(),
-                          self.height - (y + 1) * self._tileset.tiles_size_in_pixels())
+                tile.blit(i * self._tileset.tiles_size_in_pixels() - self._origin_x,
+                          self.height - (j + 1) * self._tileset.tiles_size_in_pixels() + self._origin_y)
+
+    def on_mouse_drag(self, _x: int, _y: int, dx: int, dy: int, buttons: int, _modifiers: int) -> None:
+        # Move around using the right mouse button.
+        if buttons & pyglet.window.mouse.RIGHT != 0:
+            self._origin_x = min(max(self._origin_x - dx, 0), self._level_width_in_pixels() - self.width)
+            self._origin_y = min(max(self._origin_y + dy, 0), self._level_height_in_pixels() - self.height)
+
+    def on_resize(self, width: int, height: int) -> None:
+        super().on_resize(width, height)
+        # Avoid going beyond the boundaries of the level when increasing the size of the window.
+        self._origin_x = min(self._origin_x, self._level_width_in_pixels() - width)
+        self._origin_y = min(self._origin_y, self._level_height_in_pixels() - height)
 
 
 class _TilesetWindow(pyglet.window.Window):

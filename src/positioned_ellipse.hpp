db90e22078810e0336799baf08661d12fd759811
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2022-03-23 19:50:22 +0100

    Explained that the position of an ellipse is the position of its center

--- src/positioned_ellipse.hpp
+++ src/positioned_ellipse.hpp
@@ -14,10 +14,25 @@ class PositionedEllipse {
   PositionedEllipse& operator=(PositionedEllipse&& other) = delete;
   bool contains(Position const& position) const;
   Ellipse const& ellipse() const;
+  /**
+   * @brief Returns the position of the center of the ellipse.
+   *
+   * @return Position const& the position of the center of the ellipse.
+   */
   Position const& position() const;
   int64_t radiusX() const;
   int64_t radiusY() const;
+  /**
+   * @brief Returns the position on the X axis of the center of the ellipse.
+   *
+   * @return int64_t the X position of the center of the ellipse.
+   */
   int64_t x() const;
+  /**
+   * @brief Returns the position on the Y axis of the center of the ellipse.
+   *
+   * @return int64_t the Y position of the center of the ellipse.
+   */
   int64_t y() const;
 
  private:

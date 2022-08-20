0f14a895223b4a9ce3e90223150e470de4c6deea
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2022-08-20 18:50:46 +0200

    Licensed under GPL-3.0-or-later

--- src/action_sprite_mapper.hpp
+++ src/action_sprite_mapper.hpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2021-2022 Jean-Marie BARAN (jeanmarie.baran@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Refer to 'COPYING.txt' for the full notice.
+ */
+
 #ifndef ACTION_SPRITE_MAPPER_HPP_INCLUDED
 #define ACTION_SPRITE_MAPPER_HPP_INCLUDED
 

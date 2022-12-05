dfdd0f606e3421c95a8cb8693271c55595ff5197
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2022-12-05 12:29:43 +0100

    Preparing split of `CMakeLists.txt` files and more standard project structure

--- source/media/include/media.hpp
+++ source/media/include/media.hpp
@@ -17,17 +17,11 @@
  * Refer to 'COPYING.txt' for the full notice.
  */
 
-#ifndef MODEL_HPP_INCLUDED
-#define MODEL_HPP_INCLUDED
+#ifndef MEDIA_HPP_INCLUDED
+#define MEDIA_HPP_INCLUDED
 
-#include "../action_sprite_mapper.hpp"
-#include "../animation_player.hpp"
-#include "../character.hpp"
-#include "../level.hpp"
-#include "../navigator.hpp"
-#include "../positioned_rectangle.hpp"
-#include "../spriteset.hpp"
-#include "../tile_solid_mapper.hpp"
-#include "../vector.hpp"
+#include "../src/keyboard_state.hpp"
+#include "../src/renderer.hpp"
+#include "../src/texture.hpp"
 
 #endif
--- source/model/include/model.hpp
+++ source/model/include/model.hpp
@@ -17,11 +17,17 @@
  * Refer to 'COPYING.txt' for the full notice.
  */
 
-#ifndef MEDIA_HPP_INCLUDED
-#define MEDIA_HPP_INCLUDED
+#ifndef MODEL_HPP_INCLUDED
+#define MODEL_HPP_INCLUDED
 
-#include "../keyboard_state.hpp"
-#include "../renderer.hpp"
-#include "../texture.hpp"
+#include "../src/action_sprite_mapper.hpp"
+#include "../src/animation_player.hpp"
+#include "../src/character.hpp"
+#include "../src/level.hpp"
+#include "../src/navigator.hpp"
+#include "../src/positioned_rectangle.hpp"
+#include "../src/spriteset.hpp"
+#include "../src/tile_solid_mapper.hpp"
+#include "../src/vector.hpp"
 
 #endif

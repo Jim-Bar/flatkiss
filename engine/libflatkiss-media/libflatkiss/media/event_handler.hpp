7d197a4bf0c27cc9498ff6738e5dca4bb0fe94bb
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2022-12-22 17:54:16 +0100

    Fixed use of raw array

--- engine/libflatkiss-media/libflatkiss/media/event_handler.hpp
+++ engine/libflatkiss-media/libflatkiss/media/event_handler.hpp
@@ -20,6 +20,7 @@
 #ifndef LIBFLATKISS_MEDIA_EVENT_HANDLER_HPP_INCLUDED
 #define LIBFLATKISS_MEDIA_EVENT_HANDLER_HPP_INCLUDED
 
+#include <vector>
 #include <libflatkiss/media/key.hpp>
 
 // Forward declaration to avoid exposing SDL to the outside world.
@@ -28,7 +29,7 @@
 // https://stackoverflow.com/a/1280969
 // https://stackoverflow.com/q/22386191
 // https://github.com/libsdl-org/SDL/blob/main/include/SDL3/SDL_scancode.h
-#include <SDL2/SDL.h> // FIXME: Rhaaaaaa....
+#include <SDL2/SDL.h>  // FIXME: Rhaaaaaa....
 
 class EventHandler {
  public:
@@ -37,8 +38,8 @@ class EventHandler {
   bool mustQuit() const;
 
  private:
-  bool keys_state_[Key::kMax];
-  bool must_quit_ = false;
+  std::vector<bool> keys_state_{std::vector<bool>(Key::kMax)};
+  bool must_quit_{false};
 
   void updateKeysState(SDL_Scancode key, bool pressed);
 };

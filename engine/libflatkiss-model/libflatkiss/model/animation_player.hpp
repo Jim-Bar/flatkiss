23b1e4a94a6248d562882bb2dc323006961e2a45
Jean-Marie Baran <jeanmarie.baran@gmail.com>
2023-01-10 22:08:10 +0100

    Updated copyrights for 2023

--- engine/libflatkiss-model/libflatkiss/model/animation_player.hpp
+++ engine/libflatkiss-model/libflatkiss/model/animation_player.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Jean-Marie BARAN (jeanmarie.baran@gmail.com)
+ * Copyright (C) 2021-2023 Jean-Marie BARAN (jeanmarie.baran@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- levels/levels_to_bin.py
+++ levels/levels_to_bin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2021-2022 Jean-Marie BARAN (jeanmarie.baran@gmail.com)
+# Copyright (C) 2021-2023 Jean-Marie BARAN (jeanmarie.baran@gmail.com)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
